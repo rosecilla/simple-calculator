77c7f71b6dfd165d8b4d8f1959a7d8080fa8c4f3
Your Name <priscillaore098@gmail.com>
2025-05-01 08:12:03 -0700

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+//Function() {
+   // var one = document.getElementById("num1").Value;
+   // var two = document.getElementById("num1").Value;
+    //let res = number(one) + number(two);
+   // document.getElementById("ans").innerHTML = res;
+    //console.log(res)
+//}
